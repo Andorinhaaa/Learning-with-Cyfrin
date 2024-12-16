c96f1c7a38856908459e957ec56cc13818297023
Duarte <duartespammm@gmail.com>
2024-12-16 15:23:05 +0000

    modified:   script/Interactions.s.sol 	modified:   test/Integration/InteractionTest.t.sol

--- test/Integration/InteractionTest.t.sol
+++ test/Integration/InteractionTest.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import{Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
 import{DeployFundMe} from "../../script/DeployFundMe.s.sol";
-import{FundFundMe} from "../../script/Interactions.s.sol";
+import{FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 contract InterationsTest is Test {
     FundMe fundMe;
@@ -23,11 +23,11 @@ contract InterationsTest is Test {
 
     function testUserCanFundInteractions() public{
         FundFundMe fundFundMe = new FundFundMe();
-        vm.prank(USER);
-        vm.deal(USER, SEND_VALUE);
         fundFundMe.fundFundMe(address(fundMe));
 
-        address funder = fundMe.getFunder(0);
-        assertEq(funder, USER);
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
     }
 }
